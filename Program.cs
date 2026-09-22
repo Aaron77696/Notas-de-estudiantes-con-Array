047c0aae90f742831f22bd030b98ecc3bc0c4cf6
Piero Alvarez <pieroski453@gmail.com>
2026-09-22 09:31:51 -0500

    Promedio de las notas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿int[] notas = { 18, 20, 15, 12};
+﻿using System.Diagnostics.CodeAnalysis;
+
+int[] notas = { 18, 20, 15, 12};
 
 Console.WriteLine("Notas del estudiante: ");
 for (int i = 0; i < notas.Length; i++)
 {
     Console.WriteLine($"Nota {i+1}: {notas[i]}");
 }
+//Calcular el promedio de las notas
+int suma = 0; //Suma cada nota a la variablee
+for (int i = 0; i < notas.Length; i++)
+{
+    suma += notas[i]; // Suma cada nota a la variable suma
+}
+double promedio = (double)suma/notas.Length;
+Console.WriteLine($"Promedio: {promedio:F2}"); //Calcular el promedio
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d447922660879c92c83aaa13c69a4fab429a4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd21644c6d4e2eb5d6fcb3729fd7419006fef00")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
